6fd63b25ec1129c3c4be1dc94be29be0e75de9cf
Márcio Sousa <marciosousaa2007@gmail.com>
2026-05-27 15:28:39 +0100

    sylora web site v0.58 | login: remove visitante sem conta; botões dourados: elimina efeito 3d glass, estilo flat elegante

--- login.php
+++ login.php
@@ -202,11 +202,6 @@ $csrfToken = generateCSRFToken();
 
       </form>
 
-      <div class="auth-divider"><span>ou continua como</span></div>
-      <a href="/" class="btn btn-ghost btn-block">
-        <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
-        Visitante (sem conta)
-      </a>
 
     </div>
   </div>
